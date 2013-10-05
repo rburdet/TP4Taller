e2e094da2de26fa7d7a7cd5f72123039332767e4
rburdet <rodrigoburdet@gmail.com>
2013-10-05 03:51:32 -0300

    Se encapsula el creador de cliente este ademas genera la conexion

--- common_connection.h
+++ common_connection.h
@@ -1,8 +1,12 @@
 #ifndef CLIENT_FACTORY_H
 #define CLIENT_FACTORY_H
 
+#include <iostream>
+
+#define BACKLOG 10
+
 class Connection{
-	private:
+	protected:
 		struct sockaddr_in my_addr;
 		int sd;
 	public:
@@ -12,9 +16,22 @@ class Connection{
 			}
 			my_addr.sin_family = AF_INET;
 			my_addr.sin_port = htons(port);
-			my_addr.sin_addr.s_addr = inet_addr(ip);
+			my_addr.sin_addr.s_addr = inet_addr(ip.c_str());
 			memset(&(my_addr.sin_zero), '\0', 8);
+			if (bind(sd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr))== -1) {
+				std::cerr<<"error on binding"<<std::endl;
+			}
+			if (listen(sd, BACKLOG) == -1) {
+				std::cerr<< "error on listening"<<std::endl;
+			}
+			//int yes=1;
+			//if (setsockopt(sd,SOL_SOCKET,SO_REUSEADDR, &yes,sizeof(int)) ==-1){
+			//	perror("setsockopt");
+			//	exit(1);
+			//}
 		}
+		int getSocket(){return this->sd;}
+		sockaddr_in* getAddr(){return &my_addr;}
 };
 
 #endif
--- server.cpp
+++ server.cpp
@@ -10,45 +10,33 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include "common_com.h"
+#include "server_connection.h"
 #define MYPORT 3490
  // Puerto al que conectarán los usuarios
-#define BACKLOG 10
  // Cuántas conexiones pendientes se mantienen en cola
 
 int main(void){
-	int sd, new_fd;
-	struct sockaddr_in my_addr;
+	int sd;
+	int new_fd;
 	struct sockaddr_in their_addr; 
 	socklen_t sin_size;
-//	struct sigaction sa;
-	int yes=1;
 
 	//Se crea el socket
-	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("socket");
-		exit(1);
-	}
-	//Se llena el struct sockadd_in que dsp se usa para dsp castearlo a sockaddr y usarlo en bind y listen
-	my_addr.sin_family = AF_INET;
-	my_addr.sin_port = htons(MYPORT);
-	my_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	memset(&(my_addr.sin_zero), '\0', 8);
+	//if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+	//	perror("socket");
+	//	exit(1);
+	//}
+	////Se llena el struct sockadd_in que dsp se usa para dsp castearlo a sockaddr y usarlo en bind y listen
+	//my_addr.sin_family = AF_INET;
+	//my_addr.sin_port = htons(MYPORT);
+	//my_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	//memset(&(my_addr.sin_zero), '\0', 8);
 
 	//cierra puertos que quedan abiertos
-	if (setsockopt(sd,SOL_SOCKET,SO_REUSEADDR, &yes,sizeof(int)) ==-1){
-		perror("setsockopt");
-		exit(1);
-	}
-
+	ServerConnection serv("127.0.0.1",MYPORT);
+	sd=serv.getSocket();
 	//Hace el bind 2
-	if (bind(sd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr))== -1) {
-		perror("bind");
-		exit(1);
-		}
-	if (listen(sd, BACKLOG) == -1) {
-		perror("listen");
-		exit(1);
-	}
+	
 	//while(1) { // main accept() loop
 		{
 		sin_size = sizeof(struct sockaddr_in);
@@ -62,6 +50,7 @@ int main(void){
 		if (!fork()) { // Este es el proceso hijo
 			close(sd); // El hijo no necesita este descriptor
 		Msg* toSend = createMsg("HOLISSSasdasdS");
+
 		if (sendAll(new_fd,toSend)==-1);
 		//if (send(new_fd, "Hello, world!\n", 14, 0) == -1)
 			perror("send");
