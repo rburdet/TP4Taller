22fc04c9359d2cecd911bcc955dd74e82a10d7b9
rburdet <rodrigoburdet@gmail.com>
2013-10-07 07:48:00 -0300

    CONTEMPLAR EL CASO DEL ERROR VER ESA COSA RARA DEL ORDEN

--- client_connection.cpp
+++ client_connection.cpp
@@ -10,12 +10,14 @@ int ClientConnection::startconnection(const std::string& fileName){
 	}else{//Me pude conectar
 		std::cout << "[SERVIDOR] PUERTO " << port << 
 			" Aceptado. Recibiendo datos..." << std::endl;
-		std::cout << "[CLIENTE] Enviado datos..."<<std::endl;
+		std::cout << "[CLIENTE] Enviando datos..."<<std::endl;
 		uint32_t dataSent = this->sendFile(fileName);
 		if (getData(sd)>0){
-		std::cout << Converter::convert
-	("[SERVIDOR] Datos recibidos exitosamente. Cantidad de bytes recibidos: ",
-	 dataSent)<<std::endl;
+		std::string msg=Converter::convert(
+	"[SERVIDOR] Datos recibidos exitosamente. Cantidad de bytes recibidos: ",
+	dataSent);
+		msg.push_back('.');
+		std::cout<<msg<<std::endl;
 		}
 	return 0;}
 }
