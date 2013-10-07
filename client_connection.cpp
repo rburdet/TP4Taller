fa91f8b7faa298d8c0a4982f369b309edf1cfd06
rburdet <rodrigoburdet@gmail.com>
2013-10-07 07:10:12 -0300

    mas prolijidad

---
+++ client_connection.cpp
@@ -0,0 +1,47 @@
+#include "common_converter.h"
+#include "client_connection.h"
+#include <string>
+
+int ClientConnection::startconnection(const std::string& fileName){
+	if (connect(sd, (struct sockaddr*)&my_addr, 
+				sizeof(struct sockaddr)) == -1 ){
+		perror("problemas conectado");
+		return -1;
+	}else{//Me pude conectar
+		std::cout << "[SERVIDOR] PUERTO " << port << 
+			" Aceptado. Recibiendo datos..." << std::endl;
+		std::cout << "[CLIENTE] Enviado datos..."<<std::endl;
+		uint32_t dataSent = this->sendFile(fileName);
+		if (getData(sd)>0){
+		std::cout << Converter::convert
+	("[SERVIDOR] Datos recibidos exitosamente. Cantidad de bytes recibidos: ",
+	 dataSent)<<std::endl;
+		}
+	return 0;}
+}
+
+uint32_t ClientConnection::sendFile(const std::string& fileName){
+	std::stringstream ss;
+	std::ifstream inputFile;
+	std::istream* in = &std::cin;
+	if (fileName!=""){
+		inputFile.open(fileName.c_str());
+		//Si no lo pude abrir
+		if (!inputFile.good()){
+			return 0;
+		}else{
+			in = &inputFile;
+		}
+	}	
+	char c;
+	c = in->get();
+	while(in->good()){
+		ss << c;
+		c = in->get();
+	}
+	Msg toSend(ss.str());
+	if (toSend.sendAll(sd)==-1)
+		return 0;
+	return toSend.getLength();
+
+}
--- client_connection.h
+++ client_connection.h
@@ -9,48 +9,8 @@
 class ClientConnection : public Connection{
 	public:
 		ClientConnection(const std::string& ip, int port) : Connection(ip, port){}
-
-		int startconnection(const std::string& fileName){
-		if (connect(sd, (struct sockaddr*)&my_addr, sizeof(struct sockaddr)) == -1 ){
-		perror("problemas conectado");
-		return -1;
-	}else{//Me pude conectar
-		std::cout << "[SERVIDOR] PUERTO " << port << 
-			" Aceptado. Recibiendo datos..." << std::endl;
-		std::cout << "[CLIENTE] Enviado datos..."<<std::endl;
-		uint32_t dataSent = this->sendFile(fileName);
-		if (getData(sd)>0){
-		std::cout << Converter::convert
-	("[SERVIDOR] Datos recibidos exitosamente. Cantidad de bytes recibidos: ",
-	 dataSent)<<std::endl;
-		}
-	return 0;}
-		}
-
-		uint32_t sendFile(const std::string& fileName){
-			std::stringstream ss;
-			std::ifstream inputFile;
-			std::istream* in = &std::cin;
-			if (fileName!=""){
-				inputFile.open(fileName.c_str());
-				//Si no lo pude abrir
-				if (!inputFile.good()){
-					return 0;
-				}else{
-					in = &inputFile;
-				}
-			}	
-			char c;
-			c = in->get();
-			while(in->good()){
-				ss << c;
-				c = in->get();
-			}
-			Msg toSend(ss.str());
-			if (toSend.sendAll(sd)==-1)
-				return 0;
-			return toSend.getLength();
-		}
+		int startconnection(const std::string& fileName);
+		uint32_t sendFile(const std::string& fileName);
 };
 
 #endif
